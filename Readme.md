5a75f7dd8480d0637456e4bed14094554887eed5
Theethawat Savastham <5910110150@psu.ac.th>
2019-12-05 21:43:09 +0700

    List Port

--- Readme.md
+++ Readme.md
@@ -1,10 +1,24 @@
 # Registration Mock API
+
+## Port
+
+- Main Program Input (Express) => `8080`
+- Zero MQ Server Handout => `8090`
+- Zero MQ Server Handin => `3090`
+- Main Program Output (Express) , Socket IO=> `3030`
+- Front Page (React) => `3000`
+
 ## Demo Program
+
 - Visit this API on http://dev.theduckcreator.in.th:8080 can use API Client like curl , Postman or Browser Call
+
 ## Program Specification
+
 - Program Run at Port8080 using command `npm start`
 - `/subject` to request all subject
 - `/subject/subjectCode` to request subject per code
+
 ## Method that want integration
-- Need to Make Server Connect to MongoDB Database and Frost Infrastructure 
+
+- Need to Make Server Connect to MongoDB Database and Frost Infrastructure
 - This is the mockup api that use for mock up the User Interface
--- main.js
+++ main.js
@@ -1,44 +1,78 @@
-var app = require('express')()
+/**-------Express Dependencies----------- */
+var app = require("express")()
 var port = 8080
-var faculty = require('./faculty')
-var bodyParser = require('body-parser')
+var faculty = require("./faculty")
+var bodyParser = require("body-parser")
+const EventEmitter = require("events")
+/**---------ZeroMQ Dependencies ---------------- */
+const zmq = require("zeromq")
+var zmqPushSock = new zmq.Push()
+var zeromqPushSocket = require("./pushEvent")
+/**---------Function------------------ */
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*')
-	res.header('Access-Control-Allow-Methods','POST,GET,PUT,PATCH,DELETE,OPTIONS')
-	res.header('Access-Control-Allow-Headers','Content-Type,Option,Authorization')
-	next()
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Methods",
+    "POST,GET,PUT,PATCH,DELETE,OPTIONS"
+  )
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Option,Authorization"
+  )
+  next()
 })
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-	extended: true
-}))
-app.get('/', (req, res) => {
-	res.send('Welcome to API Server')
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
+
+/**-----Data Fetching-------- */
+app.get("/", (req, res) => {
+  res.send("Welcome to API Server")
 })
 
-app.get('/all/', (req, res) => {
-	console.log('User Request All Subject')
-	res.json(faculty.findAll())
+app.get("/all/", (req, res) => {
+  console.log("User Request All Subject")
+  res.json(faculty.findAll())
 })
-app.get('/:faculty', (req, res) => {
-	var thisFaculty = req.params.faculty
-	res.json(faculty.findByFaculty(thisFaculty))
+app.get("/:faculty", (req, res) => {
+  var thisFaculty = req.params.faculty
+  res.json(faculty.findByFaculty(thisFaculty))
 })
-app.get('/:faculty/:subject', (req, res) => {
-	var thisFaculty = req.params.faculty
-	var subject = req.params.subject
-	res.json(faculty.findBySubject(thisFaculty, subject))
-})
-
-app.post('/register/',(req,res)=>{
-	let json = req.body;
-	console.log('post request has arrive at the server')
-	console.log(json)
-	res.send("Register " + json.StudentID + "complete")
-	res.end()
+app.get("/:faculty/:subject", (req, res) => {
+  var thisFaculty = req.params.faculty
+  var subject = req.params.subject
+  res.json(faculty.findBySubject(thisFaculty, subject))
 })
 
+/**----Registration---------------- */
+app.post("/register/", (req, res) => {
+  //Input using type JSON
+  let input = req.body
+  console.log("post request has arrive at the server")
+  res.send("Register " + input.StudentID + "is come to process")
+  console.log("User Want to Enroll " + input.SubjectToEnroll.length)
+  console.log(input.SubjectToEnroll)
+  //Push to Message Queue
+  zmqPushSock.bind("tcp://127.0.0.1:3030").then(() => {
+    pusherEventLoop.emit("push", input.StudentID, input.SubjectToEnroll)
+  })
+  res.end()
+})
+
+/**-----ZeroMQ Request handle--------------- */
+pusherEventLoop.on("push", (student, subject) => {
+  let dataObj = {
+    StudentID: student,
+    SubjectToEnroll: subject
+  }
+
+  zmqPushSock.send(JSON.stringify(dataObj))
+})
+
+/**-------Port Listening------- */
 app.listen(port, () => {
-	console.log('App is run at port ' + port)
+  console.log("App is run at port " + port)
 })
-
